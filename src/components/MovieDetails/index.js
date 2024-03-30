ae81d0b04a1e7fd8f1ac64adaa12d0c354b6d7f9
brodiewebdt <81633541+brodiewebdt@users.noreply.github.com>
2024-03-30 08:15:42 -0400

    Initial commit

---
+++ src/components/MovieDetails/index.js
@@ -0,0 +1,17 @@
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+
+function MovieDetails({ movies }) {
+  const { movieId } = useParams();
+  console.log(movieId);
+
+  const movieChoice = movies.find((movie) => movie.id.toString() === movieId);
+  return (
+    <div className="comp purple">
+      <h1>{movieChoice.title}</h1>
+      <p>{movieChoice.description}</p>
+    </div>
+  );
+}
+
+export default MovieDetails;
+
---
+++ src/components/Movies/index.js
@@ -0,0 +1,29 @@
+import {
+  Switch,
+  Route,
+  NavLink,
+} from "react-router-dom/cjs/react-router-dom.min";
+
+import MovieDetails from "../MovieDetails";
+
+function Movies({ movies }) {
+  return (
+    <div className="comp orange">
+      <h1>Movies Component</h1>
+      <nav>
+        {movies.map((movie) => (
+          <NavLink key={movie.id} to={`/movies/${movie.id}`}>
+            {movie.title}
+          </NavLink>
+        ))}
+      </nav>
+      <Switch>
+        <Route path="/movies/:movieId">
+          <MovieDetails movies={movies} />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default Movies;
